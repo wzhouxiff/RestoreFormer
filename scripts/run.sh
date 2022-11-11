608a81e2b1003f45c4f215b3aa70d6f096a4d244
wzhouxiff <wzhoux@mail2.sysu.edu.cn>
2022-11-11 14:28:49 +0800

    Modify the param root-path

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,13 +3,15 @@ export BASICSR_JIT=True
 conf_name='HQ_Dictionary'
 # conf_name='RestoreFormer'
 
+ROOT_PATH='' # The path for saving model and logs
+
 gpus='0,1,2,3'
 
 #P: pretrain SL: soft learning
 node_n=1
 
 python -u main.py \
---root-path /mnt/lustre/wangzhouxia/Data_t1/checkpoints/Taming/release \
+--root-path $ROOT_PATH \
 --base 'configs/'$conf_name'.yaml' \
 -t True \
 --postfix $conf_name \
